006bde4e560b2369aab57c6f2009a49436a3b0d3
MaksymIzotov <maksym.izotov02@gmail.com>
2023-03-14 16:23:25 +0900

    attack animation tweaks

--- 2DDungeonCrawler/Assets/Scripts/Enemy/EnemyHealthbar.cs
+++ 2DDungeonCrawler/Assets/Scripts/Enemy/EnemyHealthbar.cs
@@ -15,12 +15,12 @@ public class EnemyHealthbar : MonoBehaviour
 
     private void UpdateHealthbar()
     {
-        currentValue = Mathf.Clamp(currentValue, 0, 0.5f);
-        healthBar.localScale = new Vector3(Mathf.Lerp(healthBar.localScale.x, currentValue, 2f * Time.deltaTime), 0.06f, 1);
+        currentValue = Mathf.Clamp(currentValue, 0, 0.3f);
+        healthBar.localScale = new Vector3(Mathf.Lerp(healthBar.localScale.x, currentValue, 4f * Time.deltaTime), 0.03f, 1);
     }
 
     public void UpdateHealthbarValue(float maxHp, float currentHp)
     {
-        currentValue = ExtensionMethods.Remap(currentHp, 0, maxHp, 0, 0.5f);
+        currentValue = ExtensionMethods.Remap(currentHp, 0, maxHp, 0, 0.3f);
     }
 }
